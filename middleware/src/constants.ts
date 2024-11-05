36c26d2d595f0be838811e1d4e3b79a314dc8dca
romannjoroge <roman.njoroge@njuguna.com>
2024-11-05 19:46:47 +0300

    middle of encryption

--- middleware/src/constants.ts
+++ middleware/src/constants.ts
@@ -1,2 +1,5 @@
 export const SETTINGS_STORE = "settings";
 export const MIDDLEWARE_CLOCK = "clock";
+export const PRIVATE_KEY = "key";
+export const SALT_KEY = "salt";
+export const NONCE_KEY = "nonce2";
--- middleware/src/types.ts
+++ middleware/src/types.ts
@@ -8,3 +8,13 @@ export const initSchema = z.object({
         address: z.string()
     }))
 })
+
+export const passwordSchema = z.object({
+   email: z.string().optional(),
+   password: z.string(),
+   note: z.string().optional(),
+   service: z.string(),
+   username: z.string().optional() 
+})
+
+export type PasswordType = z.infer<typeof passwordSchema>;
