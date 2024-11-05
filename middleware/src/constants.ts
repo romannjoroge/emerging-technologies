85da527f6b4a2c5609aa52a782b04c8d1fdf0092
romannjoroge <roman.njoroge@njuguna.com>
2024-11-05 18:13:02 +0300

    init client route done

--- middleware/src/constants.ts
+++ middleware/src/constants.ts
@@ -1,1 +1,2 @@
 export const SETTINGS_STORE = "settings";
+export const MIDDLEWARE_CLOCK = "clock";
--- middleware/src/index.ts
+++ middleware/src/index.ts
@@ -1,5 +1,9 @@
 import "dotenv/config";
 import Express from "express";
+import store from "./store";
+import { MIDDLEWARE_CLOCK } from "./constants";
+import { initSchema } from "./types";
+import database from "./database";
 
 const app = Express()
 
@@ -7,9 +11,26 @@ const app = Express()
 //@ts-ignore
 app.post("/initialize", (req, res) => {
     try {
-        return res.status(201).json({message: "Middleware set up successfully"})
-    } catch(err) {
+        const parsed = initSchema.safeParse(req.body);
+        if (parsed.success) {
+            const data = parsed.data
+            let {clock, clientName, neighbours} = data;
+            clock[clientName] = 0;
+
+            // Store the current vector clock key somewhere
+            store.store(MIDDLEWARE_CLOCK, JSON.stringify(clock));
+
+            // Store neighbours as well somewhere
+            neighbours.forEach((ne) => {
+                database.storeNeighbour(ne.name, ne.address);
+            })
+            return res.status(201).json({ message: "Middleware set up successfully" })
+        } else {
+            return res.status(401).json({err: parsed.error});
+        }
+    } catch (err) {
         console.log("Error Initializing Middleware => ", err);
+        return res.status(500).json({err: "Internal Server Error"})
     }
 })
 
