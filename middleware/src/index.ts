46fedfa46f68e184bb3e9b709b90c7125f490b6c
romannjoroge <roman.njoroge@njuguna.com>
2024-11-05 16:31:23 +0300

    feat: Placeholder for endpoints

--- middleware/src/index.ts
+++ middleware/src/index.ts
@@ -3,6 +3,26 @@ import Express from "express";
 
 const app = Express()
 
+//@ts-ignore
+app.get("/get", (req, res) => {
+    return res.send("GET")
+});
+
+//@ts-ignore
+app.post("/add", (req, res) => {
+    return res.status(201).send("ADD");
+});
+
+//@ts-ignore
+app.delete("/delete", (req, res) => {
+    return res.status(201).send("DELETE");
+});
+
+//@ts-ignore
+app.put("/update", (req, res) => {
+    return res.status(201).send("UPDATE");
+})
+
 app.listen(process.env.PORT, () => {
     console.log(`Server is listening on port ${process.env.PORT}...`)
 })
