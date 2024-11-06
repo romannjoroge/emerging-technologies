2a8dc2762345eb67f450d39ce82e42c4ce088e61
romannjoroge <roman.njoroge@njuguna.com>
2024-11-06 21:59:37 +0300

    feat: Reimplementing encrypt functionality

--- middleware/src/constants.ts
+++ middleware/src/constants.ts
@@ -1,5 +1,2 @@
 export const SETTINGS_STORE = "settings";
-export const MIDDLEWARE_CLOCK = "clock";
-export const PRIVATE_KEY = "key";
-export const SALT_KEY = "salt";
-export const NONCE_KEY = "nonce2";
+export const MIDDLEWARE_CLOCK = "clock";;
