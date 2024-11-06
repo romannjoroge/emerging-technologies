b5230e455750edbbaf3a2cb97cdb39c642d39ff8
romannjoroge <roman.njoroge@njuguna.com>
2024-11-06 22:50:58 +0300

    feat: Functions to get and store password

--- middleware/src/database/index.ts
+++ middleware/src/database/index.ts
@@ -1,9 +1,20 @@
 import sqlite3 from "sqlite3";
+import encrypt from "../crypto";
+import { PasswordType } from "../types";
 
 interface Neighbour {
     id: number,
     name: string,
     address: string
+}
+
+interface RawPassword {
+    id: number,
+    email?: string,
+    password: Buffer,
+    note?: string,
+    service: string
+    username?: string
 }
 
 interface Password {
@@ -12,7 +23,7 @@ interface Password {
     password: string,
     note?: string,
     service: string
-    usename?: string
+    username?: string
 }
 
 class Database {
@@ -21,7 +32,7 @@ class Database {
         try {
             this.db.serialize(() => {
                 this.db.run("CREATE TABLE IF NOT EXISTS neighbours (id INTEGER PRIMARY KEY, name TEXT NOT NULL, address TEXT NOT NULL)");
-                this.db.run("CREATE TABLE IF NOT EXISTS passwords (id INTEGER PRIMARY KEY, service TEXT NOT NULL, password TEXT NOT NULL, username TEXT, email TEXT, aob TEXT)");
+                this.db.run("CREATE TABLE IF NOT EXISTS passwords (id INTEGER PRIMARY KEY, service TEXT NOT NULL, password BLOB NOT NULL, username TEXT, email TEXT, aob TEXT)");
             })
         } catch (err) {
             console.log("Could Not Create Database => ", err);
@@ -61,20 +72,31 @@ class Database {
 
     getPassword(service: string): Promise<Password> {
         return new Promise((res, rej) => {
-            this.db.get("SELECT id, email, password, aob as note, service, username FROM passwords WHERE service = ? ORDER BY id DESC LIMIT 1 ", [service], (err, row: Password[]) => {
+            this.db.get("SELECT id, email, password, aob as note, service, username FROM passwords WHERE service = ? ORDER BY id DESC LIMIT 1 ", [service], (err, row: RawPassword) => {
                 if(err) {
                     console.log("Error Getting Password From Database => ", err);
                     return rej(new Error("Could Not Get Password"));
                 }
-
-                return res(row[0])
+                return res({
+                  id: row.id,
+                  email: row.email,
+                  password: encrypt.decrypt(row.password),
+                  note: row.note,
+                  service: row.service,
+                  username: row.username
+                })
             })
         })
     }
 
-    storePassword(service: string) {
+    storePassword(password: PasswordType) {
         try {
-            
+            // Encrypt password
+            const encryptedPassword = encrypt.encrypt(password.password);
+
+            // Store details in DB
+            this.db.run("INSERT INTO passwords (service, password, username, email, aob) VALUES (?, ?, ?, ?, ?)", 
+                [password.service, encryptedPassword, password.username, password.email, password.note])
         } catch(err) {
             console.log("Error Storing Password => ", err);
             throw "Error Storing Password";
